7309457063b599bbca8e98d2c7102226d63f7c33
unknown <nguye453@cougars.csusm.edu>
2021-05-21 14:02:02 -0700

    finished gauge chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -33,7 +33,57 @@ function updateMetadata(sample)
       metaPanel.append("h6").text(`${key.toUpperCase()}: ${value}`)
     })
     
+    // Gauge Chart
+    var data = [
+      {
+        domain: { x: [0, 1], y: [0, 1] },
+        title: { text: '<b>Belly Button Washing Frequency</b><br><sub>Scrubs per Week</sub><br>'},
+        value: result.wfreq,
+        type: "indicator",
+        mode: "gauge",
+        gauge: {
+          axis: { range: [null,9], tickwidth: 1 },
+          steps: [
+            { range: [0, 1], color: "white" },
+            { range: [1, 2], color: "A6D9E7" },
+            { range: [2, 3], color: "7BC6DA" },
+            { range: [3, 4], color: "5EBAD3" },
+            { range: [4, 5], color: "48AAC5" },
+            { range: [5, 6], color: "3794AD" },
+            { range: [6, 7], color: "25829B" },
+            { range: [7, 8], color: "177189" },
+            { range: [8, 9], color: "13647A" },
+          ],
+          threshold: 
+          {
+            line: 
+            { 
+              color: "purple", 
+              width: 4 
+            },
+            thickness: 0.75,
+            value: result.wfreq
+          }
+        }
+      }
+    ];
   
+    var layout = 
+    {
+      width: 450,
+      height: 400,
+      margin: { t: 25, r: 25, l: 25, b: 25 },
+      line: { color: 'green' },
+      xaxis: 
+      {
+        showticklabels: true,
+        tickmode: 'linear',
+        tick0: 0,
+        dtick: 1
+      }
+    };
+  
+    Plotly.newPlot("gauge", data, layout);
   });
 }
 
